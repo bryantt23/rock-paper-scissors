f2d6fdf97b1fd96c804a4659ab3461d2241c9e67
bryant tunbutr <bryantt23@users.noreply.github.com>
2020-12-20 19:00:07 -0800

    Add UI

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 const options = ['Rock', 'Paper', 'Scissors'];
+let playerWins = 0,
+  computerWins = 0;
+const neededWins = 2;
 
 function computerPlay() {
   return options[Math.floor(Math.random() * 3)];
@@ -16,32 +19,75 @@ function playRound(playerSelection, computerSelection) {
     return choicesMadeMessage + drawMessage;
   } else if (playerSelectionLowerCase === 'rock') {
     if (computerSelectionLowerCase === 'paper') {
+      computerWins++;
       return choicesMadeMessage + computerWinsMessage;
     } else {
+      playerWins++;
       return choicesMadeMessage + playerWinsMessage;
     }
   } else if (playerSelectionLowerCase === 'paper') {
     if (computerSelectionLowerCase === 'scissors') {
+      computerWins++;
       return choicesMadeMessage + computerWinsMessage;
     } else {
+      playerWins++;
       return choicesMadeMessage + playerWinsMessage;
     }
   } else {
     //scissors
     if (computerSelectionLowerCase === 'rock') {
+      computerWins++;
       return choicesMadeMessage + computerWinsMessage;
     } else {
+      playerWins++;
       return choicesMadeMessage + playerWinsMessage;
     }
   }
 }
 
-function game() {
-  for (let i = 0; i < 5; i++) {
-    let playerSelection = prompt('Rock, paper, or scissors');
-    const computerSelection = computerPlay();
-    console.log(playRound(playerSelection, computerSelection));
+let buttons = document.querySelectorAll('button');
+console.log(buttons);
+buttons.forEach(button => {
+  button.addEventListener('click', () => {
+    console.log(button.innerText);
+    const playerSelection = button.innerText;
+    game(playerSelection);
+  });
+});
+
+function game(playerSelection) {
+  const computerSelection = computerPlay();
+  const gameResult = playRound(playerSelection, computerSelection);
+  tallyWins();
+  const resultDiv = document.querySelector('#game-result');
+  const p = document.createElement('p');
+  p.textContent = gameResult;
+  if (resultDiv.hasChildNodes()) {
+    resultDiv.removeChild(resultDiv.firstChild);
   }
+  resultDiv.appendChild(p);
 }
 
-game();
+function tallyWins() {
+  const totalWins = document.querySelector('#total-wins');
+  const p = document.createElement('p');
+  p.textContent = `Player has ${playerWins} wins and the computer has ${computerWins} wins`;
+
+  if (playerWins === neededWins || computerWins === neededWins) {
+    const gameArea = document.querySelector('#game');
+    const gameResult = document.querySelector('#game-result');
+    gameArea.style = 'display:none';
+    if (playerWins === neededWins) {
+      p.textContent = 'Player wins';
+    } else {
+      p.textContent = 'Computer wins';
+    }
+    gameResult.appendChild(p);
+    totalWins.removeChild(totalWins.firstChild);
+  } else {
+    if (totalWins.hasChildNodes()) {
+      totalWins.removeChild(totalWins.firstChild);
+    }
+    totalWins.appendChild(p);
+  }
+}
